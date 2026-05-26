f1f3819c97c373272f795fd86b1369417322a46c
Ivandelucak <ivandelucakanina@gmail.com>
2026-05-26 09:35:37 -0300

    fix

--- src/app/[categorySlug]/page.tsx
+++ src/app/[categorySlug]/page.tsx
@@ -437,9 +437,11 @@ export default async function CategoryPage({ params }: CategoryPageProps) {
             </div>
 
             <div className="grid gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-              {serializedProducts.map((product) => (
-                <ProductCard key={product.id} product={product} />
-              ))}
+              {serializedProducts.map(
+                (product: (typeof serializedProducts)[number]) => (
+                  <ProductCard key={product.id} product={product} />
+                ),
+              )}
             </div>
           </>
         ) : (
